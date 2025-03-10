21bf5000dc39f132dbcc0c56a81736781fe3aa3a
raymondchoy14 <raymondchoy14@gmail.com>
2025-03-10 20:20:23 +1100

    Add files via upload

---
+++ Task1.4P/js/script.js
@@ -0,0 +1,8 @@
+
+// JavaScript file linked to index.html
+
+// Function to display a message when the button is clicked
+function displayMessage() {
+    document.getElementById("message").innerText = "Hello, SIT725! You have successfully executed JavaScript.";
+    console.log("Message displayed successfully.");
+}
