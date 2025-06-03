3094dc98e211b990dc40855404aa89f1d89f0c18
raymondchoy14 <raymondchoy14@gmail.com>
2025-06-03 14:44:59 +1000

    Updated roomController to match MVC workshop style

--- Task5.2C/controllers/roomController.js
+++ Task5.2C/controllers/roomController.js
@@ -2,5 +2,10 @@ const roomService = require('../services/roomService');
 
 exports.getAllRooms = (req, res) => {
   const rooms = roomService.getAllRooms();
-  res.json({ data: rooms });
+
+  res.json({
+    status: 200,
+    data: rooms,
+    message: 'Room list retrieved using service'
+  });
 };
